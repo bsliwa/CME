f5f767b8fff43ece931851d7acfe8f68f62cab83
Cloud User <ec2-user@ip-172-31-18-13.us-west-2.compute.internal>
2018-05-12 22:44:59 +0000

    more updates

--- common/cmechatLogger.C
+++ common/cmechatLogger.C
@@ -1,5 +1,31 @@
+#include <iostream>
+
 #include "cmechatLogger.h"
 
-cmechatLogger::cmechatLogger(char *destFileName)
+cmechatLogger::cmechatLogger(char *destFileName) :
+	_fd(0),
+	_isGood(false)
 {
+	if (destFileName == 0)
+	{
+		std::cout << "ERROR: destFileName is NULL. Could not start the file logger." << std::endl;
+		return;
+	}
+
+	_fd = open(destFileName, O_APPEND | O_CREAT | O_WRONLY);
+	if (_fd <= 0)
+	{
+		std::cout << "ERROR: Name is NULL. Could not init the file logger." << std::endl;
+		return;
+	}
+
+	_isGood = true;
+
+	
+}
+
+
+cmechatLogger::~cmechatLogger()
+{
+//	if (
 }
--- common/cmechatLogger.h
+++ common/cmechatLogger.h
@@ -1,9 +1,19 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 class cmechatLogger
 {
 public:
+	bool isGood() const { return _isGood; }
+
 	cmechatLogger(char *destFile);
+	~cmechatLogger();
 
 private:
+	int _fd;
+	bool _isGood;
+
 	cmechatLogger();
+	void setIsGood(bool b) { _isGood = b; }
 };
